948ee2640f2bc19447ea7cfa3c14da6c540292f5
vanessh <vanesa.tot@student.um.si>
2023-05-03 10:09:15 +0200

    Added formulas for calculating data of a triangle.

--- client/src/components/TriangleInput.vue
+++ client/src/components/TriangleInput.vue
@@ -1,31 +1,72 @@
 <template>
-    <div>
-      <label for="base-input">Base:</label>
-      <label for="area-output">Area:</label>
-      <span id="area-output">{{ area }}</span>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'TriangleInput',
-    mounted() {
-    console.log(this.$route.path); // Access the path property of the route object
+  <div>
+    <h2>Parametri trikotnika:</h2>
+    <form @submit.prevent="calculateTriangleData">
+      <label>
+        Višina (h):
+        <input type="number" v-model.number="height" min="0" max="200" :class="{'is-invalid': !validateHeight}" required>
+        <div class="invalid-feedback" v-if="!validateHeight">Višina mora biti med 0 in 200.</div>
+      </label>
+      <br>
+      <label>
+        Stranica (a):
+        <input type="number" v-model.number="a" min="0" max="200" :class="{'is-invalid': !validateLengthA}" required>
+        <div class="invalid-feedback" v-if="!validateLengthA">Dolžina stranice mora biti med 0 in 200.</div>
+      </label>
+      <br>
+      <button class="btn btn-primary" type="submit" :disabled="!isValid">Izračunaj</button>
+    </form>
+    <CalculatedData v-if="shapeData"></CalculatedData>
+  </div>
+</template>
+
+<script>
+import CalculatedData from './CalculatedData.vue';
+export default {
+  name: 'TriangleInput',
+  components: {
+    CalculatedData
   },
-    props: {
-      base: {
-        type: Number,
-        required: true,
-      },
-      height: {
-        type: Number,
-        required: true,
-      },
-      area: {
-        type: Number,
-        required: true,
-      },
+  props: ['shapeType'],
+  data() {
+    return {
+      height: null,
+      a: null,
+    };
+  },
+  methods: {
+    calculateTriangleData() {
+      const o = parseFloat(this.a) * 3; //Obseg osnovne ploskve
+      const O = (Math.pow(parseFloat(this.a), 2) * Math.sqrt(3)) / 4; //Površina osnovne ploskve
+      const V = O * (this.height); //Volumen
+      const shapeData = {
+        type: this.shapeType,
+        height: parseFloat(this.height),
+        a: parseFloat(this.a),
+        obseg: o,
+        povrsina: O,
+        volumen: V
+      };
+      this.$emit('shape-calculated', shapeData)
+      /*this.result = (this.height * this.a) / 2;
+      const shapeData = {
+        type: this.shapeType,
+        height: this.height,
+        a: this.a,
+      };*/
+      console.log(shapeData);
+    },
+  },
+  computed: {
+    isValid(){
+      return  this.height >=0 && this.height <= 200 && this.a >= 0 && this.a <= 200 ;
+    },
+    validateHeight() {
+      return this.height >=0 && this.height <= 200;
+    },
+    validateLengthA() {
+      return this.a >= 0 && this.a <= 200;
     }
-  };
-  </script>
-  
+  }
+};
+</script>
